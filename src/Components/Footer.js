2c1de2c0beadbc89541e91d263436e95cc46360f
Skogstad <thor-bakke@hotmail.com>
2019-09-27 22:29:47 +0200

    final push

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,12 @@ const Footer = () => {
         <section className="row">
           <section className="col-3-3">
             <Card className="grow">
-              <h1>FOOTER</h1>
+              <h4>
+                This app is made for educational purposes. I take no
+                responsibility for how you use application or the information
+                you found here. Use at your own risk. This product uses the
+                Spotify API.
+              </h4>
             </Card>
           </section>
         </section>
